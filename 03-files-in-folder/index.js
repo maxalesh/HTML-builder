2d51cdf01d0bfa98163eea6efd36f4097cb28a53
Maxim Alyoshin <maxim.alyosh@gmail.com>
2024-01-19 15:06:22 +0500

    refactor: reduce size of printInfoFiles() func

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,22 +2,18 @@ const fsPromises = require('node:fs/promises');
 const path = require('path');
 
 async function printInfoFiles(pathDir) {
-  try {
-    const files = await fsPromises.readdir(path.join(pathDir), {
-      withFileTypes: true,
-    });
-    for (const file of files) {
-      if (file.isFile()) {
-        const extName = path.extname(file.name);
-        const pathFile = path.resolve(`${pathDir}\\${file.name}`);
-        const fileStat = await fsPromises.stat(pathFile);
-        const fileName = path.basename(file.name, extName);
+  const files = await fsPromises.readdir(path.join(pathDir), {
+    withFileTypes: true,
+  });
+  for (const file of files) {
+    if (file.isFile()) {
+      const extName = path.extname(file.name);
+      const pathFile = path.resolve(`${pathDir}\\${file.name}`);
+      const fileStat = await fsPromises.stat(pathFile);
+      const fileName = path.basename(file.name, extName);
 
-        console.log(`${fileName} - ${extName.slice(1)} - ${fileStat.size}b`);
-      }
+      console.log(`${fileName} - ${extName.slice(1)} - ${fileStat.size}b`);
     }
-  } catch (err) {
-    console.error(err);
   }
 }
 
